e5e23c3b95f8e6a67b417c50d8a3885b6599bbb5
WannaBeChicken <prathamdavbistupur@gmail.com>
2021-12-01 09:01:29 +0530

    Follow and unfollow working flawlessly

--- network/urls.py
+++ network/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
     path("<str:user_id>", views.profile , name="profile"),
 ]
